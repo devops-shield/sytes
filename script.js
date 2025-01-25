60af0e8e3181b19cf4175a032af93d7ba4d08350
root <root@pop-os.localdomain>
2025-01-25 17:30:06 +0300

    add url

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById('lang-en').addEventListener('click', function() {
     // Устанавливаем активную кнопку
     setActiveButton('en');
     // Перенаправление на англоязычный сайт
-    window.location.href = "https://example.com"; // Замените на нужный URL
+    window.location.href = "https://devops-shield.sytes.net/en/"; // Замените на нужный URL
 });
 
 document.getElementById('lang-ru').addEventListener('click', function() {
@@ -36,7 +36,7 @@ const countdownTimer = setInterval(function() {
     if (timeRemaining <= 0) {
         clearInterval(countdownTimer);
         setActiveButton('en');
-        window.location.href = "https://example.com"; // Замените на нужный URL
+        window.location.href = "https://devops-shield.sytes.net/en/"; // Замените на нужный URL
     }
 }, 1000); // 1000 миллисекунд = 1 секунда
 
